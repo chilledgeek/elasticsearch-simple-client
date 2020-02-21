8a52b0d16c5e30bf75328b48c23a20835f666d3e
CHILLED GEEK <chilledgeek@gmail.com>
2020-02-21 18:34:35 +0000

    make setup actually install

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-import setuptools
+from setuptools import setup, find_packages
 
-setuptools.setup(name='elasticsearch_simple_client',
-                 version="0.0.1",
-                 author="E CHOW",
-                 author_email="chilledgeek@gmail.com",
-                 description='Interact to any elasticsearch server, with simple upload and search functionality',
-                 url="https://github.com/chilledgeek/elasticsearch-simple-client",
-                 install_requires=['elasticsearch', 'pandas'])
+setup(name='elasticsearch_simple_client',
+      version="0.0.1",
+      author="E CHOW",
+      author_email="chilledgeek@gmail.com",
+      description='Interact to any elasticsearch server, with simple upload and search functionality',
+      url="https://github.com/chilledgeek/elasticsearch-simple-client",
+      packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+      license="MIT",
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ],
+      install_requires=['elasticsearch', 'pandas'])
